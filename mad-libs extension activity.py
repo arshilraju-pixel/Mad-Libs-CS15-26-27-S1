7d6f4d79dae234ae6dda5a0069630008663fa0fb
arshilraju-pixel <arshilraju@gmail.com>
2026-09-04 10:48:08 -0600

    Create mad-libs extension activity.py

---
+++ mad-libs extension activity.py
@@ -0,0 +1,11 @@
+subject = input("subject?")
+food = input("food?")
+verb = input("Verb?")
+name = input("name?")
+number = input("number?")
+animal = input("animal?")
+adjective = input("adjective?")
+
+message = (f'During {subject} class, {name} walked into the room carrying a {adjective} {animal}. Suddenly, the animal started {verb} and launched {number} {food} across the classroom. {name} said, /"WHAT IS GOING ON!/"')
+
+print(message)
