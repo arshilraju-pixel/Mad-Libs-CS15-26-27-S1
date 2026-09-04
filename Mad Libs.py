1965ec239a42f0cb2d8b5fecf35ca1ad256d4ea0
arshilraju-pixel <arshilraju@gmail.com>
2026-09-04 10:28:27 -0600

    Create Mad Libs.py

---
+++ Mad Libs.py
@@ -0,0 +1,11 @@
+noun = input("noun?")
+adjective = input("adjective?")
+verb = input("Verb?")
+name = input("name?")
+number = input("number?")
+plural_noun = input("plural_noun?")
+adjective_2 = input("adjective_2?")
+
+message = (f'Today in computer science class, Mr Forsyth taught us how to program a(n) {noun}. First we clicked the {adjective} button, then the computer started {verb}. Suddenly, {name} accidentally created {number} {plural_noun}! Mr. Forsyth said, /"This is the most {adjective_2} bug Ive ever seen./"')
+
+print(message)
